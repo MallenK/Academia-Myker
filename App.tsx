61665508a65e869e835249b90eca5c7a7ba66b74
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2025-11-25 15:01:26 +0100

    Add HorariosModalidades component to App

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Features from './components/Features';
 import Courses from './components/Courses';
+import HorariosModalidades from './components/Horarios-Modalidades';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -17,6 +18,7 @@ function App() {
         <About />
         <Features />
         <Courses />
+        <HorariosModalidades />
         <Testimonials />
         <Contact />
       </main>
