5de45703eb4e4c3df9320d4c3ca8dce169bb1b42
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2025-11-27 17:19:10 +0100

    Create OpinionesPage component with testimonials and CTA

---
+++ pages/OpinionesPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Testimonials from '../components/Testimonials';
+import CTAFinal from '../components/CTAFinal';
+
+const OpinionesPage: React.FC = () => {
+  return (
+    <>
+      <Testimonials />
+      <CTAFinal />
+    </>
+  );
+};
+
+export default OpinionesPage;
