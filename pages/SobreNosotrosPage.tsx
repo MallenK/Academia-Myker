da2883553e606aa87f7b57ba350e6932d36ca1c3
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2025-11-27 18:02:47 +0100

    Update SobreNosotrosPage.tsx

--- pages/SobreNosotrosPage.tsx
+++ pages/SobreNosotrosPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from '../components/About';
 import Features from '../components/Features';
+import Profesores from '../components/Profesores';
 import Testimonials from '../components/Testimonials';
 import CTAFinal from '../components/CTAFinal';
 
@@ -8,6 +9,7 @@ const SobreNosotrosPage: React.FC = () => {
   return (
     <>
       <About />
+      <Profesores />
       <Features />
       <Testimonials />
       <CTAFinal />
