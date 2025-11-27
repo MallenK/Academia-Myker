d0146d91590ca86e2ec75be498d67ccaf26a7be3
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2025-11-27 17:19:26 +0100

    Create ContactoPage component with FAQ and Contact

---
+++ pages/ContactoPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import FAQ from '../components/FAQ';
+import Contact from '../components/Contact';
+import CTAFinal from '../components/CTAFinal';
+
+const ContactoPage: React.FC = () => {
+  return (
+    <>
+      <FAQ />
+      <Contact />
+      <CTAFinal />
+    </>
+  );
+};
+
+export default ContactoPage;
