04fd4725a30bc8a38a53887cbc1f063d864b00b9
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2025-11-27 17:52:03 +0100

    Update MetodologiaPage.tsx

--- pages/MetodologiaPage.tsx
+++ pages/MetodologiaPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import About from '../components/About';
 import MetodologiaNiveles from '../components/MetodologiaNiveles';
 import Certificaciones from '../components/Certificaciones';
 import CTAFinal from '../components/CTAFinal';
@@ -7,7 +6,6 @@ import CTAFinal from '../components/CTAFinal';
 const MetodologiaPage: React.FC = () => {
   return (
     <>
-      <About />
       <MetodologiaNiveles />
       <Certificaciones />
       <CTAFinal />
