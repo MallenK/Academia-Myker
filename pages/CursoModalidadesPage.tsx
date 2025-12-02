bdcd4ac2a713cebe56adf433113f346864e40484
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2025-12-02 10:34:12 +0100

    Create CursoModalidadesPage.tsx

---
+++ pages/CursoModalidadesPage.tsx
@@ -0,0 +1,16 @@
+import CoursePageTemplate from "../components/CoursePageTemplate";
+import { useTranslation } from "react-i18next";
+
+export default function CursoModalidades() {
+  const { t } = useTranslation();
+
+  return (
+    <CoursePageTemplate
+      title={t("courseDetail.french.title")}
+      description={t("courseDetail.french.desc")}
+    >
+      <p>{t("courseDetail.french.p1")}</p>
+      <p>{t("courseDetail.french.p2")}</p>
+    </CoursePageTemplate>
+  );
+}
