ab1f8a55804b71ca18092c5f71957f815374caf5
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2026-02-24 15:59:19 +0000

    set static basename for pages

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ import CursoRepasoPage from './pages/CursoRepasoPage';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/Academia-Myker/">
       <div className="min-h-screen bg-gray-50 flex flex-col font-sans">
         <Header />
 
